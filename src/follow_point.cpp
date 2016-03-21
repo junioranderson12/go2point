1ccc8b04eed1d8aa82204448bf237e7753ece177
Junior Anderson <juniorars@gmail.com>
2016-03-21 11:45:37 -0300

    Second Commit

--- src/follow_point.cpp
+++ src/follow_point.cpp
@@ -9,8 +9,8 @@
 ros::Publisher twist_Pub;
 //std_msgs::Float32 msg_sonar;
 geometry_msgs::Twist velocidade;
-double x = 4;
-double y = 1.5;
+double x = -3;
+double y = -1.5;
 
 void odomCallback(const nav_msgs::OdometryConstPtr &msg)
 {
@@ -28,20 +28,31 @@ void odomCallback(const nav_msgs::OdometryConstPtr &msg)
     pt = msg->pose.pose.position;
     
     double teta = atan((y-pt.y)/(x-pt.x));
-    if (x < pt.x)
+    if ((x < pt.x) && (y > pt.y))
         teta = teta + M_PI;
+    if ((x < pt.x) && (y < pt.y))
+        teta = teta - M_PI;
     
     double delta_teta = 0.5*(teta - yaw);
     velocidade.angular.z = delta_teta;
     velocidade.linear.x = 0;
+ //   velocidade.linear.x = 0.2;
     double distancia = sqrt(pow((x-pt.x),2)+pow((y-pt.y),2));
     
-    if (delta_teta < 0.05){
+    if (delta_teta < 0)
+        delta_teta = delta_teta * (-1);
+    
+    if (delta_teta < 0.01){
         if(distancia >= 0.1)
             velocidade.linear.x = 0.2;
         else
             velocidade.linear.x = 0;
     }
+    
+
+//        if(distancia <= 0.3)
+//            velocidade.linear.x = 0;
+
     twist_Pub.publish(velocidade);
 }
     
